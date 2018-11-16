e96b6fd9354dcd7e82d07e180a338a97d2174aa9
home <home@DESKTOP-JHRJDIF>
2018-11-17 00:40:41 +0900

    관리자 데이터 추가 (비밀번호 해싱 전)

--- springSec/src/main/resources/db/schema.sql
+++ springSec/src/main/resources/db/schema.sql
@@ -6,5 +6,6 @@ name varchar(20) not null,
 power int default 0
 );
 
+insert into member(id, pw, name, power) values('master', 'admin', 'admin', 1);
 insert into member(id, pw, name) values('user01', 'asdf', 'user01');
 insert into member(id, pw, name) values('user02', 'asdf', 'user02');
