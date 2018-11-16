342a95c0d2a59108363fcd49da3f6b37a4e8ebd8
home <home@DESKTOP-JHRJDIF>
2018-11-16 22:33:06 +0900

    ver 1

--- springSec/src/main/java/com/spring/study/HomeController.java
+++ springSec/src/main/java/com/spring/study/HomeController.java
@@ -1,19 +1,16 @@
 package com.spring.study;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.spring.study.repository.MemberDao;
+import com.spring.study.service.MemberService;
 
 /**
  * Handles requests for the application home page.
@@ -22,7 +19,7 @@ import com.spring.study.repository.MemberDao;
 public class HomeController {
 	
 	@Autowired
-	private MemberDao memberDao;
+	private MemberService memberService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -30,16 +27,11 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-//		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("serverTime", memberDao.test());
+		memberService.test();
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		logger.info(user.toString());
 		
 		return "home";
 	}
--- springSec/src/main/java/com/spring/study/service/MemberService.java
+++ springSec/src/main/java/com/spring/study/service/MemberService.java
@@ -2,7 +2,12 @@ package com.spring.study.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service("memberService")
 public class MemberService {
-
+	public void test() {
+		log.info("test log √‚∑∞");
+	}
 }
